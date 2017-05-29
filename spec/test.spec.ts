1ca7a642c1492fdfa9f167f7d7d7030f3a21dcd3
James Ots <code@jamesots.com>
2017-05-29 23:55:24 +0100

    Make peek command work

--- spec/test.spec.ts
+++ spec/test.spec.ts
@@ -513,16 +513,18 @@ start:
     it('should format 2 digit hex', function() {
         zat.loadProg(prog);
 
+        zat.load('00X', 'line')
+
         zat.z80.a = 0x12;
         zat.z80.hl = zat.getAddress('line');
         zat.call('format_hex_2');
 
-        expect(zat.getMemory('line', 2)).toEqual(stringToBytes('12'));
+        expect(zat.getMemory('line', 3)).toEqual(stringToBytes('12X'));
 
         zat.z80.a = 0xaf;
         zat.z80.hl = zat.getAddress('line');
         zat.call('format_hex_2');
 
-        expect(zat.getMemory('line', 2)).toEqual(stringToBytes('AF'));
+        expect(zat.getMemory('line', 3)).toEqual(stringToBytes('AFX'));
     });
 });
